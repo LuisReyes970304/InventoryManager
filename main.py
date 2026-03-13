9209b46a9c1f5c432be66757b933b085202a80b0
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-03-13 08:13:49 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#This is the main file in which run all the program. 
+from model import InventoryManager
+
+app = InventoryManager()
+
+welcome_message = """
+
+******************** Welcome to InventoryManger ********************
+
+********** This is a simple program that works in console **********
+
+********************** Les't start  with this **********************
+
+"""
+
+print(welcome_message)
+
---
+++ model.py
@@ -0,0 +1,43 @@
+#This is the main model for objects!
+
+#This is the model that create the product dict
+class Inventory:
+    def __init__(self):
+        self.name = input("Provide the product name: ")
+        self.price = float(input(f"Provide the {self.name}'s price: "))
+        self.amount = int(input(f"Provide the {self.name}'s amount: "))
+
+        self.subtotal = self.price * self.amount
+
+    def create_dict(self):
+        return {
+            "name": self.name,
+            "price": self.price,
+            "amount": self.amount,
+            "subtotal": self.subtotal
+        }
+    
+
+class InventoryManager(Inventory):
+    def __init__(self):
+        super().__init__()
+        self.product_list = []
+        
+    def add_to_list(self):
+        product_dict = self.create_dict()
+        self.product_list.append(product_dict)
+        return "\nProduct data added to the database list\n"
+    
+    def show_total(self):
+        for i in range(len(self.product_list)):
+            info = self.product_list[i]
+            print("Product number:", (i + 1), "\n")
+            print("Product name: ...............:",info["name"])
+            print("Product price: ..............:",info["price"])
+            print("Product cuantity: ..............:",info["amount"])
+            print("Product subtotal: ...........:",info["subtotal"])
+
+
+test = InventoryManager()
+test.add_to_list()
+test.show_total()
